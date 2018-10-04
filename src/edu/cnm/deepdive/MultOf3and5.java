ec14f22c0cab23212f7356b8df304131174f6e3b
brianb <bbleck@unm.edu>
2018-10-04 00:29:51 -0600

    Increased 100 to 1000 (the correct parameters)

--- src/edu/cnm/deepdive/MultOf3and5.java
+++ src/edu/cnm/deepdive/MultOf3and5.java
@@ -5,13 +5,13 @@ public class MultOf3and5 {
 
   public static void main(String[] args) {
     int sum = 0;
-    for (int i = 1; i < 100; i++) {
+    for (int i = 1; i < 1000; i++) {
       if(i%3==0 ||i%5==0){
         System.out.println(i);
         sum+=i;
       }
     }
-    System.out.println(sum);
+    System.out.println("Sum: " + sum);
   }
 
 }
