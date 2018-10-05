3d1843a2143982f10af764a23136077b35f0efe1
brianb <bbleck@unm.edu>
2018-10-05 08:14:38 -0600

    Pulled magic numbers into static final ints.

--- src/edu/cnm/deepdive/MultOf3and5.java
+++ src/edu/cnm/deepdive/MultOf3and5.java
@@ -2,12 +2,15 @@ package edu.cnm.deepdive;
 
 public class MultOf3and5 {
 
+  static final int ATHOUSAND = 1000;
+  static final int NUM_ONE = 3;
+  static final int NUM_TWO = 5;
+
 
   public static void main(String[] args) {
     int sum = 0;
-    for (int i = 1; i < 1000; i++) {
-      if(i%3==0 ||i%5==0){
-        System.out.println(i);
+    for (int i = 1; i < ATHOUSAND; i++) {
+      if(i%NUM_ONE==0 ||i%NUM_TWO==0){
         sum+=i;
       }
     }
